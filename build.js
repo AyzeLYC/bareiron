5d50941f3dd55171e7f651689c04c8aa109cbe08
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-09-13 09:13:58 +0200

    Updated build.js

--- build.js
+++ build.js
@@ -1,8 +1,30 @@
+const build_configuration = {
+
+    "os": "ubuntu"
+    
+};
+
 function run_command(arguments) {
     
     let { spawn } = require("node:child_process");
     
     let shell_process = spawn(arguments, {detached: true, shell: true, windowsHide: true}),
         result = "";
+
+    shell_process.stdout.on("data", function(data) {
+
+        result += data;
+        
+    };
+    shell_process.exit();
+
+    return result;
     
+};
+
+if (build_configuration["os"].toLowerCase() === "debian" || build_configuration["os"].toLowerCase() === "ubuntu") {
+    
+    run_command(["sudo", "apt", "update", "-y", "&&", "sudo", "apt", "upgrade", "-y"]); // udpates all the server apps
+    run_command(["sudo", "apt", ""]);
+
 };
