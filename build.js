674ea63925b3c4f1f752eb5b9f74e63b79aae064
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-09-19 10:46:20 +0200

    Updated build.js

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const build_configuration = {
 
     "os": "ubuntu", // change this with the name of your operating system
-    "server_dot_jar_url": ""
+    "server_dot_jar_url": "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar"
     
 };
 
