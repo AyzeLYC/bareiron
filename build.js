f27bb5f151a8520094da574aef24a03ab7b876d1
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-09-19 11:59:44 +0200

    Updated build.js

--- build.js
+++ build.js
@@ -56,7 +56,7 @@ var serverdotjar_data = ""; // creates a new variable that will contain the .jar
 if (String(build_configuration["os"]).toLowerCase() === "debian" || String(build_configuration["os"]).toLowerCase() === "ubuntu") {
     
     run_command("sudo apt update -y && sudo apt upgrade -y"); // udpates all the server apps
-    run_command("sudo apt install gcc default-jre -y"); // install the latest release of gcc and java
+    run_command("sudo apt install clang default-jre -y"); // install the latest release of gcc and java
     
     run_jar_and_build_server(); // launch the build function
 
@@ -65,7 +65,7 @@ if (String(build_configuration["os"]).toLowerCase() === "debian" || String(build
 if (String(build_configuration["os"]).toLowerCase() === "centos" || String(build_configuration["os"]).toLowerCase() === "fedora" || String(build_configuration["os"]).toLowerCase() === "rhel") {
 
     run_command("sudo dnf update -y && sudo dnf upgrade -y");
-    run_command("sudo dnf install gcc default-jre -y");
+    run_command("sudo dnf install clang default-jre -y");
     
     run_jar_and_build_server();
     
@@ -74,7 +74,7 @@ if (String(build_configuration["os"]).toLowerCase() === "centos" || String(build
 if (String(build_configuration["os"]).toLowerCase() === "centos-old" || String(build_configuration["os"]).toLowerCase() === "fedora-old" || String(build_configuration["os"]).toLowerCase() === "rhel-old") {
 
     run_command("sudo yum update -y && sudo yum upgrade -y");
-    run_command("sudo yum install gcc default-jre -y");
+    run_command("sudo yum install clang default-jre -y");
     
     run_jar_and_build_server();
     
@@ -83,7 +83,7 @@ if (String(build_configuration["os"]).toLowerCase() === "centos-old" || String(b
 if (String(build_configuration["os"]).toLowerCase() === "arch" || String(build_configuration["os"]).toLowerCase() === "msys2") {
 
     run_command("sudo pacman update -y && sudo pacman upgrade -y");
-    run_command("sudo pacman -S gcc jdk-openjdk -y");
+    run_command("sudo pacman -S clang jdk-openjdk -y");
     
     run_jar_and_build_server();
     
@@ -92,7 +92,7 @@ if (String(build_configuration["os"]).toLowerCase() === "arch" || String(build_c
 if (String(build_configuration["os"]).toLowerCase() === "opensuse" || String(build_configuration["os"]).toLowerCase() === "suse-enterprise") {
 
     run_command("sudo zypper update -y && sudo zypper upgrade -y");
-    run_command("sudo zypper install gcc java-17-openjdk-devel -y");
+    run_command("sudo zypper install clang java-17-openjdk-devel -y");
     
     run_jar_and_build_server();
     
@@ -101,7 +101,7 @@ if (String(build_configuration["os"]).toLowerCase() === "opensuse" || String(bui
 if (String(build_configuration["os"]).toLowerCase() === "gentoo") {
 
     run_command("sudo emerge --update --deep world -y && sudo emerge --upgrade -y");
-    run_command("sudo emerge gcc ");
+    run_command("sudo emerge clang ");
 
     run_jar_and_build();
     
