2d6d7b9646bb16adc26ceba5157cd15601355dfc
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-09-25 18:06:26 +0200

    Updated build-ECMAScript.js

--- build-ECMAScript.js
+++ build-ECMAScript.js
@@ -6,7 +6,7 @@ const __dirname = import.meta.dirname;
 const build_configuration = {
 
     "os": "ubuntu", // change this with the name of your operating system
-    "server_file_url": "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar"
+    "server_file_url": "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar" /* 1.21.8 server jar file url */
     
 };
 
