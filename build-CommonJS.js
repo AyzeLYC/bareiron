13b0aec68fc758a1867240e031c43d71d1b76224
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-10-03 13:54:18 +0200

    Updated build-CommonJS.js

--- build-CommonJS.js
+++ build-CommonJS.js
@@ -22,7 +22,7 @@ function run_command(command) {
     let child_process = require("node:child_process");
 
     let command_arguments = command_datas[],
-        shell_process = child_process.spawn(command.split("0")[0], command_arguments, {detached: true, shell: true, windowsHide: true}),
+        shell_process = child_process.spawn(command.split(" ")[0], command_arguments, {detached: true, shell: true, windowsHide: true}),
         command_result = ``;
 
     Object.values(command).forEach(function(value, index, array) {
